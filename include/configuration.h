fd44b1c333872c787a4b9aeb24a26132c41c6e21
Bril999 <brildima1@gmail.com>
2025-12-09 12:17:46 +0300

    реализация пинг-понга, появились проблемы с spi

--- include/configuration.h
+++ include/configuration.h
@@ -11,16 +11,24 @@
 #include "xprintf.h"
 #include "mik32_hal_irq.h"
 #include "mik32_hal_gpio.h"
+//#include <math.h>
 
 /* === Глобальные параметры === */
+#define MAX_VALUES 128
+
 extern uint16_t tim32_top;
 extern uint8_t dac_div;
-extern uint16_t values_quantity;
-extern uint32_t word_src[20];
+extern uint8_t values_quantity;
+extern uint16_t word_src[MAX_VALUES];
+extern uint16_t max_value;
+extern uint16_t min_value;
+extern volatile uint8_t dma_last_started;     // 0 -> a был запущен, 1 -> b был запущен
+extern volatile uint8_t dma_busy;             // защита от повторного старта
 
 extern uint8_t signal_form;
-extern uint16_t freq;
-extern uint8_t amplitude;
+extern uint8_t freq;
+extern uint8_t start_ampl;
+extern uint8_t finish_ampl;
 
 /* === Хэндлы периферии === */
 extern SPI_HandleTypeDef hspi0;
@@ -28,6 +36,7 @@ extern TIMER32_HandleTypeDef htimer32;
 extern TIMER32_CHANNEL_HandleTypeDef htimer32_channel;
 extern DMA_InitTypeDef hdma;
 extern DMA_ChannelHandleTypeDef hdma_ch0;
+extern DMA_ChannelHandleTypeDef hdma_ch1;
 extern DAC_HandleTypeDef hdac1;
 
 /* === Инициализации === */
--- src/main.c
+++ src/main.c
@@ -1,12 +1,10 @@
 #include "configuration.h"
-#include "mik32_hal_irq.h"
-#include "uart_lib.h"
-#include "xprintf.h"
 
 //#define PI 3.14159265358979323846
 
 uint8_t slave_output[4];
 uint8_t slave_input[4];
+uint8_t dma_active_channel = 0;
 
 void parse_SPI_parametrs(void);
 void generate_signal(uint8_t signal);
@@ -26,11 +24,6 @@ int main(void)
     HAL_Timer32_Value_Clear(&htimer32);
     HAL_Timer32_Start(&htimer32);
     
-
-    HAL_EPIC_MaskLevelSet(HAL_EPIC_SPI_0_MASK);
-    HAL_IRQ_EnableInterrupts();
-
-    
     xprintf("MIK32 SPI Slave (IRQ) example start\n");
 
     SPI0_Init();
@@ -39,88 +32,96 @@ int main(void)
        HAL_SPI_Init() не обязательно включает модуль; включаем явно. */
     HAL_SPI_Enable(&hspi0);
 
-    for (int i = 0; i < values_quantity; i++) word_src[i] = (4095 * i) / (values_quantity - 1); //повыносить в переменные
-
+    for (int i = min_value; i < values_quantity; i++) {
+        word_src[i] = (max_value * i) / (values_quantity - 1);
+        //xprintf("%d \n", word_src[i]);
+    }
+    HAL_EPIC_MaskLevelSet(HAL_EPIC_SPI_0_MASK | HAL_EPIC_DMA_MASK);
+    HAL_IRQ_EnableInterrupts();
+    HAL_DMA_Start(&hdma_ch0, (void *)&word_src, (void *)&hdac1.Instance_dac->VALUE, values_quantity*sizeof(uint16_t) - 1);
+    
     while (1)
     {
-        HAL_DMA_Start(&hdma_ch0, (void *)&word_src, (void *)&hdac1.Instance_dac->VALUE, sizeof(word_src) - 1); 
-        if (HAL_DMA_Wait(&hdma_ch0, 10 * DMA_TIMEOUT_DEFAULT) != HAL_OK); //обязательно для корректной работы DMA
-        /* Если SPI готов — начинаем обмен в прерывном режиме.
-           HAL_SPI_Exchange_IT установит hspi0.State != READY пока идёт обмен. */
+        // HAL_DMA_Start(&hdma_ch0, (void *)&word_src, (void *)&hdac1.Instance_dac->VALUE, values_quantity*sizeof(uint16_t) - 1);
+        // if (HAL_DMA_Wait(&hdma_ch0, DMA_TIMEOUT_DEFAULT) != HAL_OK); //обязательно для корректной работы DMA
+        //xprintf("status = %d\n", HAL_DMA_GetChannelReadyStatus(&hdma_ch0));
         if (hspi0.State == HAL_SPI_STATE_READY)
         {
             HAL_StatusTypeDef SPI_Status = HAL_SPI_Exchange_IT(&hspi0, slave_output, slave_input, sizeof(slave_output));
             if (SPI_Status != HAL_OK)
             {
-                /* Что-то пошло не так — сбросим ошибки и вернём состояние */
-                xprintf("HAL_SPI_Exchange_IT returned error\n");
                 HAL_SPI_ClearError(&hspi0);
                 hspi0.State = HAL_SPI_STATE_READY;
             }
-        }
-
-        /* Обработка завершённого обмена */
-        if (hspi0.State == HAL_SPI_STATE_END)
-        {
-            xprintf("SPI IRQ: RX completed:\n");
-            xprintf(" form = %02X, freg = %04X, ampl = %02X\n", signal_form, freq, amplitude);
-
-            /* Подготовиться к следующему обмену */
-            hspi0.State = HAL_SPI_STATE_READY;
-        }
-
-        /* Обработка ошибок */
-        if (hspi0.State == HAL_SPI_STATE_ERROR)
-        {
-            xprintf("SPI_Error: OVR %d, MODF %d\n",
-                    (hspi0.ErrorCode & HAL_SPI_ERROR_OVR) ? 1 : 0,
-                    (hspi0.ErrorCode & HAL_SPI_ERROR_MODF) ? 1 : 0);
-
-            HAL_SPI_ClearError(&hspi0);
-        /* В примере референса SPI временно выключают — можно сделать так же */
-            HAL_SPI_Disable(&hspi0);
-            HAL_DelayMs(1);
-            HAL_SPI_Enable(&hspi0);
-            hspi0.State = HAL_SPI_STATE_READY;
         }
     }
 }
 
 void trap_handler()
-{
+{   
     if (EPIC_CHECK_SPI_0())
     {
         HAL_SPI_IRQHandler(&hspi0);
         parse_SPI_parametrs();
         generate_signal(signal_form);
-    }
 
+        if (hspi0.State == HAL_SPI_STATE_END)
+        {
+            /* Подготовиться к следующему обмену */
+            hspi0.State = HAL_SPI_STATE_READY;
+        }
+    }
+    
+    if (EPIC_CHECK_DMA())
+    {
+        if (dma_active_channel == 0)
+        {
+            HAL_DMA_ClearLocalIrq(&hdma);
+            // for (uint32_t i = 0; i < 4; i++)
+            // {
+            //     xprintf("elem = %d\n", word_dst[i]);
+            // }
+            HAL_DMA_Start(&hdma_ch1, (void *)&word_src, (void *)&hdac1.Instance_dac->VALUE, values_quantity*sizeof(uint16_t) - 1);
+            dma_active_channel = 1;
+        }
+        else
+        {
+            HAL_DMA_ClearLocalIrq(&hdma);
+            // for (uint32_t i = 0; i < 4; i++)
+            // {
+            //     xprintf("elem = %d\n", word_dst[i]);
+            // }
+            HAL_DMA_Start(&hdma_ch0, (void *)&word_src, (void *)&hdac1.Instance_dac->VALUE, values_quantity*sizeof(uint16_t) - 1);
+            dma_active_channel = 0;
+        }
+    }
     /* Сброс прерываний (очистка EPIC статуса) */
     HAL_EPIC_Clear(0xFFFFFFFF);
 }
 
 void parse_SPI_parametrs(void) {
     signal_form = slave_input[0];
-    freq = slave_input[1] | (slave_input[2] << 8);
-    amplitude = slave_input[3];
+    freq = slave_input[1];
+    start_ampl = slave_input[2];
+    finish_ampl = slave_input[3];
 }
 
 void generate_signal(uint8_t signal) {
     switch (signal)
     {
     case 0x01: //Пила
-        for (int i = 0; i < values_quantity; i++) word_src[i] = (4095 * i) / (values_quantity - 1);
+        for (int i = min_value; i < values_quantity; i++) word_src[i] = (max_value * i) / (values_quantity - 1);
         break;
     case 0x02: //Треугольник
-        for (int i = 0; i < values_quantity; i++) word_src[i] = (i < values_quantity/2)
-              ?  (4095 * i)/(values_quantity/2)
-              :  (4095 * (values_quantity-i))/(values_quantity/2);
+        for (int i = min_value; i < values_quantity; i++) word_src[i] = (i < values_quantity/2)
+              ?  (max_value * i)/(values_quantity/2)
+              :  (max_value * (values_quantity-i))/(values_quantity/2);
         break;
     case 0x03: //Синус
-        // for (int i = 0; i < values_quantity; i++) word_src[i] = (sin(2*M_PI*i/values_quantity)+1) * 2047;
+        //for (int i = 0; i < values_quantity; i++) word_src[i] = (sin(2*M_PI*i/values_quantity)+1) * 2047;
         break;
     case 0x04: //Меандр
-        for (int i = 0; i < values_quantity; i++) word_src[i] = (i < values_quantity/2) ? 4095 : 0;
+        for (int i = min_value; i < values_quantity; i++) word_src[i] = (i < values_quantity/2) ? max_value : min_value;
         break;
     default:
         break;
